cfc8aecdbf7e20430cda97a7e276507905224705
Aja Singer <ajasinger@gmail.com>
2023-02-08 16:07:23 -0500

    add delete function

--- index.js
+++ index.js
@@ -15,3 +15,17 @@ async function typeSentence(word, eleRef, delay = 100) {
 function waitForMs(ms) {
   return new Promise(resolve => setTimeout(resolve, ms))
 }
+
+
+/* delete function */
+
+async function deleteSentence(eleRef) {
+  const sentence = $(eleRef).html();
+  const letters = sentence.split("");
+  let i = 0;
+  while(letters.length > 0) {
+    await waitForMs(100);
+    letters.pop();
+    $(eleRef).html(letters.join(""));
+  }
+}
