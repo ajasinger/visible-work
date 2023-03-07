bc6a1365c9e8287482cd1fbc6700cc71e555ec8f
Aja Singer <ajasinger@gmail.com>
2023-03-07 15:58:52 -0500

    define varibles for use in type function

---
+++ index.js
@@ -0,0 +1,14 @@
+const i = 0;
+const txt = ['WORK', 'EXPERIENCE', 'ACCOMPLISHMENTS'];
+
+//typing animation 
+function type() {
+  //split array into words 
+  //for i=0 loop through letters and add them to the DOM 
+  //delete them from the DOM 
+  //for i=1 etc etc  
+}
+
+//deleting animation 
+
+//how to type then delete then type next word?
